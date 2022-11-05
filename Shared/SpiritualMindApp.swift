46178a4912b947e6c1b32b7cd9eae279903dc5f2
Atika <haryouli.atika@gmail.com>
2022-11-05 12:18:23 +0100

    Initial Commit

---
+++ Shared/SpiritualMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiritualMindApp.swift
+//  Shared
+//
+//  Created by Atika Haryouli on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpiritualMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
